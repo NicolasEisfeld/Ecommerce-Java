9cdc26f92c2b45eec7bec7fd68c082c9aa1a675b
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2025-10-14 21:58:56 -0300

    Add files via upload

---
+++ src/main/java/dev/nicolas/ecommercesistema/EcommercesistemaApplication.java
@@ -0,0 +1,13 @@
+package dev.nicolas.ecommercesistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesistemaApplication.class, args);
+	}
+
+}
