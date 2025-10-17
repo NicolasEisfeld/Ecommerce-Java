dba214debe659cd8585915b9c63ccbecdc0a92e1
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2025-10-17 20:49:35 -0300

    Criando o Modelo de Estado

---
+++ src/main/java/dev/nicolas/ecommercesistema/models/EstadoModel.java
@@ -0,0 +1,28 @@
+package dev.nicolas.ecommercesistema.models;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name="estados")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstadoModel implements Serializable{
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String nome;
+
+    private String sigla; 
+}
---
+++ src/main/java/dev/nicolas/ecommercesistema/repositories/EstadoRepository.java
@@ -0,0 +1,5 @@
+package dev.nicolas.ecommercesistema.repositories;
+
+public interface EstadoRepository {
+
+}
